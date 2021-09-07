357ffc255197efa38a545c08e95b3b362f0b69fb
auddl0756 <cjddl789123@naver.com>
2021-09-07 13:27:22 +0900

    시간 보여주기 수정

--- src/main/java/com/example/demo/controller/TodayController.java
+++ src/main/java/com/example/demo/controller/TodayController.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@RestController
+@Controller
 public class TodayController {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @GetMapping("/today")
-    public String today(){
-        return formatter.format(LocalDateTime.now());
+    public String today(Model model){
+        model.addAttribute("nowTime",formatter.format(LocalDateTime.now()));
+        return "today";
     }
 }
