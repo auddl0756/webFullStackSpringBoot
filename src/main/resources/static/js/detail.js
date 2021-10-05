dcec265f837a40c02da110b13c02d1a1015b1793
auddl0756 <cjddl789123@naver.com>
2021-10-05 17:02:20 +0900

    상세 페이지 타이틀 영역 네비 버튼 동작 구현

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -15,8 +15,16 @@ async function initDetailPage() {
 
 class TitleArea {
     constructor(displayInfoId) {
+        this.width = 414;
         this.template = $('#detailTitleTemplate').html();
 
+        this.addEventListeners();
+
+    }
+
+    addEventListeners() {
+        $('.prev_inn').on('click', this.prevEvent.bind(this));
+        $('.nxt_inn').on('click', this.nextEvent.bind(this));
     }
 
     requestTitleData(displayInfoId) {
@@ -41,8 +49,44 @@ class TitleArea {
             resultHTML += bindTemplate(tData);
         }
 
-        resultHTML+=bindTemplate(titleData[0]);
+        resultHTML += bindTemplate(titleData[0]);
 
         $('.visual_img').html(resultHTML);
+
+        $('.store_details .dsc').html(titleData[0].content);
+
+        $('.num span').html(titleData.length);
+
+        //기타 이미지가 없을 때 이전,다음 이미지 화살표 지우기
+        if (titleData.length === 1) {
+            $('.prev_inn').css('display', 'none');
+            $('.nxt_inn').css('display', 'none');
+        }
+
+        $('.visual_img').css('left', '0px');
+    }
+
+    prevEvent() {
+        let nowLeft = parseInt($('.visual_img').css('left'));
+        if (nowLeft === 0) {    //첫번째 이미지를 의미
+            return;
+        }
+
+        $('.visual_img').css('left', (nowLeft + this.width) + "px");
+
+        $('.prev_inn').css('display', 'none');
+        $('.nxt_inn').css('display', 'inline-block');
+    }
+
+    nextEvent() {
+        let nowLeft = parseInt($('.visual_img').css('left'));
+        if (nowLeft === -this.width) {  //마지막 이미지를 의미
+            return;
+        }
+
+        $('.visual_img').css('left', (nowLeft - this.width) + "px");
+
+        $('.nxt_inn').css('display', 'none');
+        $('.prev_inn').css('display', 'inline-block');
     }
 }
