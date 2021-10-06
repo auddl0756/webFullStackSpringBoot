af6788b63e6572a91d7a969d8cc549d9512a9644
auddl0756 <cjddl789123@naver.com>
2021-10-06 14:16:37 +0900

    상세 페이지 타이틀 영역, 중복된 jQuery 선택자 재활용하도록 수정

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -18,18 +18,23 @@ class TitleArea {
         this.width = 414;
         this.template = $('#detailTitleTemplate').html();
 
-        this.addEventListeners();
+        this.titleImageSection = $('.visual_img');
 
-        this.moreDescriptionButton = $('.store_details');
+        this.prevButton = $('.prev_inn');
+        this.nextButton = $('.nxt_inn');
+
+        this.moreDescriptionButton = $('.bk_more');
+        this.descriptionSection = $('.store_details');
         this.openDescriptionButton = $('#open_button');
         this.closeDescriptionButton = $('#close_button');
 
+        this.addEventListeners();
     }
 
     addEventListeners() {
-        $('.prev_inn').on('click', this.prevEvent.bind(this));
-        $('.nxt_inn').on('click', this.nextEvent.bind(this));
-        $('.bk_more').on('click', this.descriptionEvent.bind(this));
+        this.prevButton.on('click', this.prevEvent.bind(this));
+        this.nextButton.on('click', this.nextEvent.bind(this));
+        this.moreDescriptionButton.on('click', this.descriptionEvent.bind(this));
     }
 
     requestTitleData(displayInfoId) {
@@ -56,66 +61,57 @@ class TitleArea {
 
         resultHTML += bindTemplate(titleData[0]);
 
-        $('.visual_img').html(resultHTML);
+        this.titleImageSection.html(resultHTML);
 
-        $('.store_details .dsc').html(titleData[0].content);
+        $('.store_details > .dsc').html(titleData[0].content);
 
         $('.num span').html(titleData.length);
 
-        //기타 이미지가 없을 때 이전,다음 이미지 화살표 지우기
+        //기본적으로 처음에 1번째 타이틀 이미지니까 prev 버튼 보여주지 않기.
+        this.prevButton.css('display', 'none');
+
+        //기타 이미지가 없을 때 다음 이미지 화살표 지우기
         if (titleData.length === 1) {
-            $('.prev_inn').css('display', 'none');
-            $('.nxt_inn').css('display', 'none');
+            this.nextButton.css('display', 'none');
         }
 
-        $('.visual_img').css('left', '0px');
-        $('.visual_img').css('transition', '0.5s ease-in');
+        this.titleImageSection.css('left', '0px');
+        this.titleImageSection.css('transition', '0.5s ease-in');
     }
 
     prevEvent() {
-        let nowLeft = parseInt($('.visual_img').css('left'));
+        let nowLeft = parseInt(this.titleImageSection.css('left'));
         if (nowLeft === 0) {    //첫번째 이미지를 의미
             return;
         }
 
-        $('.visual_img').css('left', (nowLeft + this.width) + "px");
+        this.titleImageSection.css('left', (nowLeft + this.width) + "px");
 
-        $('.prev_inn').css('display', 'none');
-        $('.nxt_inn').css('display', 'inline-block');
+        this.prevButton.css('display', 'none');
+        this.nextButton.css('display', 'inline-block');
     }
 
     nextEvent() {
-        let nowLeft = parseInt($('.visual_img').css('left'));
+        let nowLeft = parseInt(this.titleImageSection.css('left'));
         if (nowLeft === -this.width) {  //마지막 이미지를 의미
             return;
         }
 
+        this.titleImageSection.css('left', (nowLeft - this.width) + "px");
 
-        $('.visual_img').css('left', (nowLeft - this.width) + "px");
-
-        $('.nxt_inn').css('display', 'none');
-        $('.prev_inn').css('display', 'inline-block');
+        this.nextButton.css('display', 'none');
+        this.prevButton.css('display', 'inline-block');
     }
 
     descriptionEvent() {
-        if (this.moreDescriptionButton.hasClass('close3')) { //펼쳐지지 않은 기본 상태
-            this.moreDescriptionButton.removeClass('close3');
+        if (this.descriptionSection.hasClass('close3')) { //펼쳐지지 않은 기본 상태
+            this.descriptionSection.removeClass('close3');
             this.openDescriptionButton.css('display', 'none');
             this.closeDescriptionButton.css('display', 'block');
         } else {  //펼쳐진 상태
-            this.moreDescriptionButton.addClass('close3');
+            this.descriptionSection.addClass('close3');
             this.closeDescriptionButton.css('display', 'none');
             this.openDescriptionButton.css('display', 'block');
         }
-
-        // if($('.store_details').hasClass('close3')){ //펼쳐지지 않은 기본 상태
-        //     $('.store_details').removeClass('close3');
-        //     $('.bk_more, ._open').css('display','none');
-        //     $('.bk_more, ._close').css('display','block');
-        // }else{  //펼쳐진 상태
-        //     $('.store_details').addClass('close3');
-        //     $('.bk_more, ._open').css('display','block');
-        //     $('.bk_more, ._close').css('display','none');
-        // }
     }
 }
