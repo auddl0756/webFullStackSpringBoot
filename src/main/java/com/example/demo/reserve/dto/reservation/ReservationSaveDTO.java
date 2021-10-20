93b65d71da5aa9ad298c94966d977e9204c38bb4
auddl0756 <cjddl789123@naver.com>
2021-10-20 21:16:52 +0900

    예약 페이지 / 예약 정보 저장을 위한 dto

--- src/main/java/com/example/demo/reserve/dto/reservation/ReservationSaveDTO.java
+++ src/main/java/com/example/demo/reserve/dto/reservation/ReservationSaveDTO.java
@@ -6,15 +6,17 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ToString
 @Setter
 @Getter
-public class BookingFormRequest {
+public class ReservationSaveDTO {
     private String name;
     private String tel;
     private String email;
+    private String reservationDate;
     private int displayInfoId;
     private int productId;
     private List<FormPrice> prices;
